f866d28f4bae8edf8e4f4d095fcbf9261790fe13
Seelam Nikhil <nikhilseelam1@gmail.com>
2026-07-02 08:36:42 +0530

    Time: 96 ms (6.19%), Space: 46.9 MB (19.86%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean next(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if(i<0) return false;
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, n - 1);
+        return true;
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            swap(nums, l, r);
+            l++;
+            r--;
+        }
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+         Arrays.sort(nums);
+        List<List<Integer>> l=new ArrayList<>();
+        do{
+            List<Integer>ls=new ArrayList<>();
+            for(int x:nums) ls.add(x);
+            l.add(ls);
+        }while(next(nums));
+        return l;
+    }
+}

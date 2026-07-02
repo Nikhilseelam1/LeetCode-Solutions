6fad2a56730a1c2097610a3437e9053ba83fe465
Seelam Nikhil <nikhilseelam1@gmail.com>
2026-07-02 10:38:41 +0530

    Time: 3 ms (42.07%), Space: 46.5 MB (82.02%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean check(char [][]board,int r,int c,int n){
+        for(int i=0;i<n;i++){
+            if(board[r][i] == 'Q') return false;
+        }
+        for(int i=0;i<n;i++){
+            if(board[i][c] == 'Q') return false;
+        }
+        for (int i = r, j = c; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q') return false;
+
+        for (int i = r, j = c; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 'Q') return false;
+
+        for (int i = r, j = c; i < n && j >= 0; i++, j--)
+            if (board[i][j] == 'Q') return false;
+
+        for (int i = r, j = c; i < n && j < n; i++, j++)
+            if (board[i][j] == 'Q') return false;
+                return true;
+    }
+    public void back(List<List<String>> ans,char [][] board,int i,int n){
+        if(i==n){
+            List<String> l = new ArrayList<>();
+            for (char[] row : board) {
+                l.add(new String(row));
+            }
+            ans.add(new ArrayList<>(l));
+        }else{
+            for(int j=0;j<n;j++){
+                if(board[i][j]!='Q' && check(board,i,j,n)){
+                    board[i][j]='Q';
+                    back(ans,board,i+1,n);
+                    board[i][j]='.';
+                }
+            }
+        }
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>>ans=new ArrayList<>();
+        char[][] board = new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }        
+        back(ans,board,0,n);
+        return ans;
+    }
+}

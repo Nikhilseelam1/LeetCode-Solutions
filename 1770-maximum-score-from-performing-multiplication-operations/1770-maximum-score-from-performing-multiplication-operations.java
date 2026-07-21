5d719f222af5c54c3a506aea738b9069d49c90c2
Seelam Nikhil <nikhilseelam1@gmail.com>
2026-07-21 22:46:26 +0530

    Time: 205 ms (8.37%), Space: 281.9 MB (11.3%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -6,7 +6,6 @@ class Solution {
         if(left==n || j==m) return 0;
         if(dp[left][j]!=-1) return dp[left][j];
         int right=(n-1)-(j-left);
-        // System.out.println(left + " " + right);
         int ans1=nums[left]*mu[j] + rec(nums,mu,left+1,j+1);
         int ans2=nums[right]*mu[j] + rec(nums,mu,left,j+1);
         dp[left][j]=Math.max(ans1,ans2);
@@ -15,12 +14,23 @@ class Solution {
     public int maximumScore(int[] nums, int[] mu) {
         n=nums.length;
         m=mu.length;
-        dp=new int [n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                dp[i][j]=-1;
+        dp=new int [n+1][m+1];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int ans2=0;
+                if(j>=i){
+                    int right=(n-1)-(j-i);
+                    ans2=nums[right]*mu[j] + dp[i][j+1];
+                }
+                int ans1=nums[i]*mu[j] + dp[i+1][j+1];
+                dp[i][j]=Math.max(ans1,ans2);
             }
         }
-        return rec(nums,mu,0,0);
+        return dp[0][0];
     }
 }

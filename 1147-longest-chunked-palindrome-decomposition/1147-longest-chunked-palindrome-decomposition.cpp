e311d8ae23059f8809f2f28b5318bd4bbb722759
Seelam Nikhil <nikhilseelam1@gmail.com>
2026-07-26 10:10:48 +0530

    Time: 0 ms (100%), Space: 10 MB (59.89%) - LeetHub

--- 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.cpp
+++ 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.cpp
@@ -1,46 +1,45 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
 class Solution {
 public:
-    ll base=31;
-    ll mod=1e9+7;
-    vector<ll>pre;
-    vector<ll>power;
-    ll hash(int l,int r){
-
-        if(l==0)
-            return pre[r];
-
-        return (pre[r]-pre[l-1]*power[r-l+1]%mod+mod)%mod;
-    }
-
-    int rec(int l,int r){
-        if(l==r) return 1;
-        if(l>r) return 0;
-        for(int i=1;l+i-1<r;i++){
-            if(hash(l,l+i-1) ==  hash(r-i+1,r)){
-                return 2 + rec(l+i,r-i);
-            }
-        }
-        return 1;
-    }
     int longestDecomposition(string text) {
         int n=text.size();
-        pre.resize(n);
-        power.resize(n+1);
-        power[0]=1;
-        for(int i=1;i<=n;i++){
-            power[i]=power[i-1]*base%mod;
+        int l=0;
+        int r=n-1;
+        int cnt=0;
+        
+        while(l<r){
+            if(text[l]==text[r]){
+                cnt+=2;
+                l++;
+                r--;
+            }
+            else{
+                string f="";
+                string b="";
+                 while (l< r) {
+                  b+=text[r];
+                  f=text[l]+f;
+
+                if (f == b)
+                    break;
+
+                l++;
+                r--;
+            }
+
+            if (f == b) {
+                cnt += 2;
+                l++;
+                r--;
+            } else {
+                cnt++;
+                return cnt;
+            }
         }
-        pre[0]=text[0]-'a'+1;
-
-        for(int i=1;i<n;i++){
-
-            pre[i]=(pre[i-1]*base + (text[i]-'a'+1))%mod;
         }
-
-        for(ll it:pre) cout<<it<<" ";
-        return rec(0,n-1);
+        if(l==r){
+            cnt++;
+        }
+        return cnt;
+        
     }
 };

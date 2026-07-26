ecc7436d6be0cd5b57b93e12429d18f14cebed42
Seelam Nikhil <nikhilseelam1@gmail.com>
2026-07-26 11:47:25 +0530

    Time: 182 ms (3.54%), Space: 12.6 MB (6.29%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
     int n;
-    vector<vector<int>> dp;
-
-    int rec(int i, vector<int>& nums, int k) {
-
-        if (k == 0 && i == n)
-            return 0;
-
-        if (k == 0 || i == n)
-            return INT_MAX;
-
-        if (dp[i][k] != -1)
-            return dp[i][k];
-
-        int ans = INT_MAX;
-        int sum = 0;
-
-        for (int j = i; j < n; j++) {
-
-            sum += nums[j];
-
-            int ft = rec(j + 1, nums, k - 1);
-
-            if (ft != INT_MAX)
-                ans = min(ans, max(sum, ft));
-        }
-
-        return dp[i][k] = ans;
-    }
-
+    vector<vector<long long>>dp;
     int splitArray(vector<int>& nums, int k) {
-
-        n = nums.size();
-
-        dp.assign(n + 1, vector<int>(k + 1, -1));
-
-        return rec(0, nums, k);
+        n=nums.size();
+        dp.assign(n+1,vector<long long>(k+1,LLONG_MAX));
+        dp[n][0]=0;
+        for(int i=n;i>=0;i--){
+            for(int k1=1;k1<=k;k1++){
+                long long sum=0;
+                // dp[i][k1]=0;
+                for(int j=i;j<n;j++){
+                    sum=sum+(long long)nums[j];
+                    long long ft=dp[j+1][k1-1];
+                    if(ft!=LLONG_MAX) dp[i][k1]=min(dp[i][k1],max(sum,ft));
+                }
+            }
+        }
+        return dp[0][k];
+        
     }
 };

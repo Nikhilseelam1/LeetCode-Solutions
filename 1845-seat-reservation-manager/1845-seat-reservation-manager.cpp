149c4bafc01712053baa398abcd26d5aaca0ce62
Seelam Nikhil <nikhilseelam1@gmail.com>
2026-07-27 17:44:38 +0530

    Time: 51 ms (93.68%), Space: 150.6 MB (78.71%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
@@ -1,13 +1,21 @@
 class SeatManager {
 public:
     priority_queue<int,vector<int>,greater<int>>pq;
+    int c;
     SeatManager(int n) {
-        for(int i=1;i<=n;i++) pq.push(i);
+        c=1;
+        // for(int i=1;i<=n;i++) pq.push(i);
+        pq.push(1);
     }
     
     int reserve() {
+        // int x=pq.top();
+        // pq.pop();
+        // return x;
         int x=pq.top();
+        c++;
         pq.pop();
+        pq.push(c);
         return x;
     }
     

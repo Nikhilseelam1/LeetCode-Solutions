6e28b642cc7b602b2e49fb10e1ba2e57f8e24610
Seelam Nikhil <nikhilseelam1@gmail.com>
2026-08-10 11:50:04 +0530

    Time: 1745 ms (6.27%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,18 @@
+SELECT p.product_id, p.product_name
+FROM Product p
+WHERE EXISTS (
+    SELECT 1
+    FROM Sales s
+    WHERE s.product_id = p.product_id
+      AND s.sale_date >= '2019-01-01'
+      AND s.sale_date <= '2019-03-31'
+)
+AND NOT EXISTS (
+    SELECT 1
+    FROM Sales s
+    WHERE s.product_id = p.product_id
+      AND (
+          s.sale_date < '2019-01-01'
+          OR s.sale_date > '2019-03-31'
+      )
+);

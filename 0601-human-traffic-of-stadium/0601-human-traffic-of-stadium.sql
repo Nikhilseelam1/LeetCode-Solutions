e03220507f8b15d952c6558c91861d75384051a2
Seelam Nikhil <nikhilseelam1@gmail.com>
2026-09-02 14:24:03 +0530

    Time: 391 ms (66.33%), Space: 0B (100%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+with cte as
+(select *,
+id-row_number() over(order by id asc) as diff
+from 
+Stadium 
+where people>=100)
+
+select id,visit_date,people
+from cte
+where diff in
+(
+select 
+diff
+from 
+cte 
+group by diff
+having count(*)>=3
+order by visit_date asc)
+
